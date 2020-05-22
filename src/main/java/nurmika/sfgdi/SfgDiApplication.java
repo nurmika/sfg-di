ba7d340954dc66210306d17ce3b5d1f323a4e448
JonSlow14 <45079402+JonSlow14@users.noreply.github.com>
2020-05-23 00:09:23 +0200

    Hello World Controller, get Bean from Context

--- src/main/java/nurmika/sfgdi/SfgDiApplication.java
+++ src/main/java/nurmika/sfgdi/SfgDiApplication.java
@@ -1,13 +1,20 @@
 package nurmika.sfgdi;
 
+import nurmika.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) context.getBean("myController");
+
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
